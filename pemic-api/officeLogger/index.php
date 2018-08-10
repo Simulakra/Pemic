229cd66be5b3e309512f78090ca3e6a675ab76cd
Unknown <alicemkaris@gmail.com>
2018-08-10 14:49:35 +0300

    api

--- pemic-api/officeLogger/index.php
+++ pemic-api/officeLogger/index.php
@@ -1,35 +1,11 @@
 <?php 
-	$conn=mysqli_connect("localhost","alicem","123456","pemic");
+	
 	date_default_timezone_set ( "Europe/Istanbul" );
 	ini_set('max_execution_time', 300);
-
-
-	function status()
-    {
-    	$conn=mysqli_connect("localhost","alicem","123456","pemic");
-    	date_default_timezone_set ( "Europe/Istanbul" );
-    	$sqlgetoffice="SELECT * FROM office";
-    	$return=mysqli_query($conn,$sqlgetoffice);
-    
-    	while ($row=mysqli_fetch_array($return)) {
-	    	$now=date('G')*60;
-	    	$now+=date('i');
-	    	if ($now>$row['Time_Enter']) {
-	    		return false;
-	    	}else{
-	    		return true;
-	    	}
-    	}
-    }
-	
-	function checkout(){
-		$conn=mysqli_connect("localhost","alicem","123456","pemic");
-	}
-    
-    ///////////------GET ONLİNE USERS LİST-------\\\\\\\\\\\\\\
+///////////------GET ONLİNE USERS LİST-------\\\\\\\\\\\\\\
     $process=array(array());
 	$recentList=array();
-	exec('FOR /L %I in (1,1,50) DO PING 192.168.1.%I -n 2 -w 500');
+	exec('FOR /L %I in (1,1,40) DO PING 192.168.1.%I -n 1 -w 500');
 	exec('arp -a',$mac);
 	
 	foreach ($mac as $line) {
@@ -49,73 +25,23 @@
 			}
 		}
 	}
- 	//////////----------------------------\\\\\\\\\\\\\\\\\\\\\\
-	$oldlistquery="SELECT * FROM onlineusers";
-	$oldlist=array();
-	$qr=mysqli_query($conn,$oldlistquery);
-	while($zrow=mysqli_fetch_array($qr)){
-		$row=array();
-		$row['MAC']=$zrow['MAC'];
-		$row['L_Counter']=$zrow['L_Counter'];
-		$oldlist[]=$row;
-	}
-	$oldlistdiff=array();
-	foreach ($oldlist as $x => $value) {
-		array_push($oldlistdiff, $value['MAC']);
-	}
-	
-    //////////---------Lists---------------\\\\\\\\\\\\\\
-	 
-	/////////-----------------------------\\\\\\\\\\\\\\\\
-	/////////----------DİFF CALCULATİON----\\\\\\\\\\\\\
-	$left=array_diff($oldlistdiff, $recentList);
-	$came=array_diff($recentList, $oldlistdiff);
-	////////-------------------------------\\\\\\\\\\\\\\\\
-	$status=status();
-	$leftLength=count($left);
-	$cameLength=count($came);
-	echo "LEFT <br>";
-	foreach($left as $x => $value) {
-    	echo $value.'<br>';
-    	$sql4="SELECT L_Counter FROM onlineusers WHERE MAC='$value'";
-    	$res=mysqli_query($conn,$sql4);
-    	$counter=mysqli_fetch_array($res)['L_Counter'];
-    	$counter++;
-    	$sql3="UPDATE onlineusers SET L_Counter='$counter' WHERE MAC='$value'";
-    	mysqli_query($conn,$sql3);
-    	if ($counter==3) {
-    		$sql="INSERT INTO logs (MAC,Action,Time,Disc) VALUES ('$value','0','".date("Y-m-d H:i:s")."','$status')";
-			$sql2="DELETE FROM onlineusers WHERE MAC='$value'";
-    		mysqli_query($conn,$sql);
-    		mysqli_query($conn,$sql2);
-    	}
-    }
-	echo "CAME <br>";
-	foreach($came as $x => $value) {
-    	echo $value.'<br>';
-    	$sql="INSERT INTO logs (MAC,Action,Time,Disc) VALUES ('$value','1','".date("Y-m-d H:i:s")."','$status')";
-		$sql2="INSERT INTO onlineusers (MAC,L_Counter) VALUES ('$value','0')";
-    	mysqli_query($conn,$sql);
-    	mysqli_query($conn,$sql2);
-    }
+		$data = $recentList;
+	 $data_string = json_encode($data);
 
-    foreach ($recentList as $x => $value) {
-    	$sql="SELECT L_Counter FROM onlineusers WHERE MAC='$value'";
-    	$res=mysqli_query($conn,$sql);
-    	$counter=mysqli_fetch_array($res)['L_Counter'];
-    	if ($counter!=0) {
-    		$sql2="UPDATE onlineusers SET L_Counter='0' WHERE MAC='$value'";
-    		mysqli_query($conn,$sql2);
-    	}
-    }
-    
-	
-	
-	
+	 $url = 'http://cgnwebtasarim.com/Yavrular/Pemic/pemic-api/patates.php';
 
-    
-    
-    
-    
+	 $ch = curl_init($url);
+	  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	  curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	                     'Content-Type: application/json',
+	                 'Content-Length: ' . strlen($data_string))
+	   );
+	  $result = curl_exec($ch);
+	  curl_close($ch);
+
+	  echo $result;
+	//////////----------------------------\\\\\\\\\\\\\\\\\\\\\\
 
 ?>
